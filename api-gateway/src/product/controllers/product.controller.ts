26dc3c8191ed76e34478530e959def57a30e639f
coolmarvel <marvel97@naver.com>
2024-03-15 15:49:08 +0900

    api-gateway(product,product-category) module update

--- api-gateway/src/product/controllers/product.controller.ts
+++ api-gateway/src/product/controllers/product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Post, Put, Query, VERSION_NEUTRAL } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, VERSION_NEUTRAL } from '@nestjs/common';
 import { ProductService } from '../services/product.service';
 import { Public } from 'src/common/decorators/public.decorator';
 import { PageReqDto } from 'src/common/dtos/req.dto';
@@ -13,54 +13,74 @@ export class ProductController {
   // WooCommerce Staging Product APIs
   @Public()
   @Post('stag')
-  @ApiOperation({ summary: '단일 상품 카테고리 생성 API (스테이징)' })
-  async createProductCategory_stag() {}
+  @ApiOperation({ summary: '단일 상품 생성 API (스테이징)' })
+  async createAnProduct_stag(@Body() createProductDto) {
+    const { name, type, regular_price, description, short_description, categories, images } = createProductDto;
+    return await this.productService.createAnProduct_stag(name, type, regular_price, description, short_description, categories, images);
+  }
 
   @Public()
-  @Get('stag/:category_id')
-  @ApiOperation({ summary: '단일 상품 카테고리 조회 API (스테이징)' })
-  async retrieveProductCategory_stag() {}
+  @Get('stag/:product_id')
+  @ApiOperation({ summary: '단일 상품 조회 API (스테이징)' })
+  async retrieveAnProduct_stag(@Param() { product_id }: { product_id: string }) {
+    return await this.productService.retrieveAnProduct_stag(product_id);
+  }
 
   @Public()
   @Get('stag')
-  @ApiOperation({ summary: '상품 카테고리 리스트 조회 API (스테이징)' })
-  async listAllProductCategories_stag() {}
+  @ApiOperation({ summary: '상품 리스트 조회 API (스테이징)' })
+  async listAllProducts_stag(@Query() { page, size }: PageReqDto) {
+    return await this.productService.listAllProducts_stag(page, size);
+  }
 
   @Public()
-  @Put('stag/:category_id')
-  @ApiOperation({ summary: '단일 상품 카테고리 갱신 API (스테이징)' })
-  async updateProductCategory_stag() {}
+  @Put('stag/:product_id')
+  @ApiOperation({ summary: '단일 상품 갱신 API (스테이징)' })
+  async updateAProduct_stag(@Param() { product_id }: { product_id: string }, @Body() data: any) {
+    return await this.productService.updateAProduct_stag(product_id, data);
+  }
 
   @Public()
-  @Delete('stag/:category_id')
-  @ApiOperation({ summary: '단일 상품 카테고리 삭제 API (스테이징)' })
-  async deleteProductCategory_stag() {}
+  @Delete('stag/:product_id')
+  @ApiOperation({ summary: '단일 상품 삭제 API (스테이징)' })
+  async deleteAProduct_stag(@Param() { product_id }: { product_id: string }) {
+    return await this.productService.deleteAProduct_stag(product_id);
+  }
 
   // WooCommerce Production Product APIs
   @Public()
   @Post('prod')
-  @ApiOperation({ summary: '단일 상품 카테고리 생성 API (프로덕션)' })
-  async createProductCategory_prod() {}
+  @ApiOperation({ summary: '단일 상품 생성 API (프로덕션)' })
+  async createAnProduct_prod(@Body() createProductDto) {
+    const { name, type, regular_price, description, short_description, categories, images } = createProductDto;
+    return await this.productService.createAnProduct_prod(name, type, regular_price, description, short_description, categories, images);
+  }
 
   @Public()
-  @Get('prod/:category_id')
-  @ApiOperation({ summary: '단일 상품 카테고리 조회 API (프로덕션)' })
-  async retrieveProductCategory_prod() {}
+  @Get('prod/:product_id')
+  @ApiOperation({ summary: '단일 상품 조회 API (프로덕션)' })
+  async retrieveAnProduct_prod(@Param() { product_id }: { product_id: string }) {
+    return await this.productService.retrieveAnProduct_prod(product_id);
+  }
 
   @Public()
   @Get('prod')
-  @ApiOperation({ summary: '상품 카테고리 리스트 조회 API (프로덕션)' })
+  @ApiOperation({ summary: '상품 리스트 조회 API (프로덕션)' })
   async listAllProducts_prod(@Query() { page, size }: PageReqDto) {
     return await this.productService.listAllProducts_prod(page, size);
   }
 
   @Public()
-  @Put('prod/:category_id')
-  @ApiOperation({ summary: '단일 상품 카테고리 갱신 API (프로덕션)' })
-  async updateProductCategory_prod() {}
+  @Put('prod/:product_id')
+  @ApiOperation({ summary: '단일 상품 갱신 API (프로덕션)' })
+  async updateAProduct_prod(@Param() { product_id }: { product_id: string }, @Body() data: any) {
+    return await this.productService.updateAProduct_prod(product_id, data);
+  }
 
   @Public()
-  @Delete('prod/:category_id')
-  @ApiOperation({ summary: '단일 상품 카테고리 삭제 API (프로덕션)' })
-  async deleteProductCategory_prod() {}
+  @Delete('prod/:product_id')
+  @ApiOperation({ summary: '단일 상품 삭제 API (프로덕션)' })
+  async deleteAProduct_prod(@Param() { product_id }: { product_id: string }) {
+    return await this.productService.deleteAProduct_prod(product_id);
+  }
 }
--- api-gateway/src/product/services/product.service.ts
+++ api-gateway/src/product/services/product.service.ts
@@ -1,19 +1,91 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
+import { IProductService } from '../interfaces/product.interface';
 
 @Injectable()
-export class ProductService {
+export class ProductService implements IProductService {
   constructor(@Inject('PRODUCT_SERVICE') private client: ClientProxy) {}
 
   // WooCommerce Staging Product APIs
+  async createAnProduct_stag(name: string, type: string, regular_price: string, description: string, short_description: string, categories: object, images: object): Promise<any> {
+    const pattern = { cmd: 'createAnProduct_stag' };
+    const payload = { name, type, regular_price, description, short_description, categories, images };
+    const product = await firstValueFrom(this.client.send(pattern, payload));
+
+    return product;
+  }
+
+  async retrieveAnProduct_stag(product_id: string): Promise<any> {
+    const pattern = { cmd: 'retrieveAnProduct_stag' };
+    const payload = { product_id };
+    const product = await firstValueFrom(this.client.send(pattern, payload));
+
+    return product;
+  }
+
+  async listAllProducts_stag(page: number, size: number): Promise<any> {
+    const pattern = { cmd: 'listAllProducts_stag' };
+    const payload = { page, size };
+    const products = await firstValueFrom(this.client.send(pattern, payload));
+
+    return products;
+  }
+
+  async updateAProduct_stag(product_id: string, data: any): Promise<any> {
+    const pattern = { cmd: 'updateAProduct_stag' };
+    const payload = { product_id, data };
+    const product = await firstValueFrom(this.client.send(pattern, payload));
+
+    return product;
+  }
+
+  async deleteAProduct_stag(product_id: string): Promise<any> {
+    const pattern = { cmd: 'deleteAProduct_stag' };
+    const payload = { product_id };
+    const product = await firstValueFrom(this.client.send(pattern, payload));
+
+    return product;
+  }
 
   // WooCommerce Production Product APIs
+  async createAnProduct_prod(name: string, type: string, regular_price: string, description: string, short_description: string, categories: object, images: object): Promise<any> {
+    const pattern = { cmd: 'createAnProduct_prod' };
+    const payload = { name, type, regular_price, description, short_description, categories, images };
+    const product = await firstValueFrom(this.client.send(pattern, payload));
+
+    return product;
+  }
+
+  async retrieveAnProduct_prod(product_id: string): Promise<any> {
+    const pattern = { cmd: 'retrieveAnProduct_prod' };
+    const payload = { product_id };
+    const product = await firstValueFrom(this.client.send(pattern, payload));
+
+    return product;
+  }
+
   async listAllProducts_prod(page: number, size: number) {
     const pattern = { cmd: 'listAllProducts_prod' };
     const payload = { page, size };
     const products = await firstValueFrom(this.client.send(pattern, payload));
 
     return products;
+  }
+
+  async updateAProduct_prod(product_id: string, data: any): Promise<any> {
+    const pattern = { cmd: 'updateAProduct_prod' };
+    const payload = { product_id, data };
+    const product = await firstValueFrom(this.client.send(pattern, payload));
+
+    return product;
+  }
+
+  async deleteAProduct_prod(product_id: string): Promise<any> {
+    const pattern = { cmd: 'deleteAProduct_prod' };
+    const payload = { product_id };
+    const product = await firstValueFrom(this.client.send(pattern, payload));
+
+    return product;
   }
 }
